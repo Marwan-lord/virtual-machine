919691c7a47e31d7c812e07a8a510418dc4c12a9
Marwan Mohamed <merolokamino@gmail.com>
2024-11-07 23:52:16 +0200

    fix error message

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -84,7 +84,7 @@ fn main() -> Result<(), AsmErr> {
     let args: Vec<_> = env::args().collect();
 
     if args.len() != 2 {
-        panic!("usage: {} <input>", args[0]);
+        panic!("usage: {} <assembly file>", args[0]);
     }
 
     let file = File::open(Path::new(&args[1])).map_err(|_| AsmErr::UnknownFile)?;
